9bc82c39d292abf1f8d27c7df4a3eede5ba43fd1
euheber <euheber1@gmail.com>
2022-06-07 21:54:05 -0300

    fix: overflow in mobile

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
 const burguerMenu = document.querySelector("#burguerMenu");
 const asideMenu = document.querySelector("#aside-menu");
+const body = document.querySelector('body')
+const profileSection = document.querySelector("#profile-section");
 
 burguerMenu.addEventListener("click", () => {
-    burguerMenu.classList.toggle('rotate')
-  asideMenu.classList.toggle('active')
+  burguerMenu.classList.toggle("rotate");
+  asideMenu.classList.toggle("active");
+
+  if(asideMenu.classList.contains('active')){
+    body.style.overflow = "hidden"
+  }
+
   
 });
