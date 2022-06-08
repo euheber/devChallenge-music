650bdf9d0f5c79c8016ce2179886256eba5451d2
euheber <euheber1@gmail.com>
2022-06-07 22:12:14 -0300

    fix: mobile menu

--- script/script.js
+++ script/script.js
@@ -1,15 +1,19 @@
 const burguerMenu = document.querySelector("#burguerMenu");
 const asideMenu = document.querySelector("#aside-menu");
-const body = document.querySelector('body')
-const profileSection = document.querySelector("#profile-section");
+const body = document.querySelector("body");
+const imgContainer = document.querySelector("#img-container");
+const editContainer = document.querySelector('#edit-container')
 
 burguerMenu.addEventListener("click", () => {
   burguerMenu.classList.toggle("rotate");
   asideMenu.classList.toggle("active");
 
-  if(asideMenu.classList.contains('active')){
-    body.style.overflow = "hidden"
+  if (asideMenu.classList.contains("active")) {
+    body.style.overflow = "hidden";
+    imgContainer.style.opacity = "0";
+    editContainer.style.opacity = '0'
+  } else {
+    imgContainer.style.opacity = "1";
+    editContainer.style.opacity = '1'
   }
-
-  
 });
